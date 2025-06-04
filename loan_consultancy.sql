c99ca42b25736cec90369d278eaaa88e1605021b
Tausif Rahman <69134814+tawsif0@users.noreply.github.com>
2025-06-04 11:34:31 +0600

    database

--- loan_consultancy.sql
+++ loan_consultancy.sql
@@ -2,8 +2,8 @@
 -- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Jun 03, 2025 at 01:31 PM
+-- Host: 127.0.0.1:3306
+-- Generation Time: Jun 04, 2025 at 07:32 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -40,6 +40,8 @@ CREATE TABLE `chambers` (
 --
 
 INSERT INTO `chambers` (`chamber_id`, `application_id`, `chamber_place_name`, `chamber_address`, `monthly_income`) VALUES
+('9152e01a-016d-4cad-aa82-e46b1033f1d0', '96d6ab54-cbc7-4f3f-8892-617ad8e04ac7', 'aaaaaaaaaaaaaaa', 'aaaaaaaaa', 122333.00),
+('99d49053-8f09-4e61-88bc-00c974fe569b', '0c98c407-4bd9-419e-ac8b-6330b583388a', 'sd', 'sd', 123.00),
 ('b85f094c-6121-4f86-b5ea-1c00ab60b519', '4e618b0c-e1a5-4582-aa6c-44fb940502df', 'hatir jheel', 'kuril', 12000.00),
 ('f4bc42a3-239b-452b-8207-6efc14b5254a', '4e618b0c-e1a5-4582-aa6c-44fb940502df', 'rampura', 'rampura', 120000.00);
 
@@ -86,9 +88,15 @@ CREATE TABLE `loan_applications` (
 --
 
 INSERT INTO `loan_applications` (`id`, `loan_type`, `fullName`, `contactNo`, `requiredAmount`, `loanRequirementTime`, `presentAddress`, `existingLoan`, `paymentRegularity`, `comments`, `department`, `designation`, `organizationAddress`, `jobGrade`, `instituteAddress`, `companyAddress`, `doctor_type`, `hospitalName`, `hospitalAddress`, `bmdcAge`, `monthlySalaryFromHospital`, `bankAmount`, `cashAmount`, `bankAndCashAmount`, `createdAt`, `salary_type`, `organizationName`, `instituteName`, `companyName`) VALUES
+('0c98c407-4bd9-419e-ac8b-6330b583388a', 'Doctor', 'Tausifur Rahman', '01552577108', 12344.00, '1 month', 'sd', 'No', '', 'sd', 'sd', 'sd', '', '', '', '', 'Job Holder', 'sd', 'sd', '11', 0.00, 12.00, 0.00, 0.00, '2025-06-03 21:37:56', 'Bank Amount', '', '', ''),
 ('4e618b0c-e1a5-4582-aa6c-44fb940502df', 'Doctor', 'Tausif', '01552577108', 12000.00, '1 month', 'goran', 'No', 'Regular', 'hi', 'test', 'as', 'kuril', '7', '', '', 'Job Holder', 'city', 'city', '12', 0.00, 12000.00, 0.00, 0.00, '2025-06-03 09:17:40', 'Bank Amount', 'arbeit', '', ''),
+('512bffa0-3c6b-40d3-a0d3-c932411903fc', 'Govt. Employee', 'Tausif Rahman', '01552577108', 123.00, '1 month', 'as', 'Yes', 'Regular', 'as', 'Website Development', 'Jr. developer', 'as', 'as', '', '', NULL, '', '', '', 0.00, 12.00, 0.00, 0.00, '2025-06-04 01:46:36', 'Bank Amount', 'aq', '', ''),
+('756a2d19-1b03-487a-b240-b26d1d0b7fd4', 'Govt. Employee', 'taufiq', '01552577108', 1234.00, '1 month', 'ftyhutfhg', 'No', '', 'cdgfdgf', 'Website Development', 'Jr. developer', 'ghjhkhgjg', 'aaaaaaaaa', '', '', NULL, '', '', '', 0.00, 0.00, 8999.00, 0.00, '2025-06-04 05:13:00', 'Cash Amount', 'aq', '', ''),
+('96d6ab54-cbc7-4f3f-8892-617ad8e04ac7', 'Doctor', 'Tausif Rahman', '01552577108', 1223.00, '1 month', 'asfdasf', 'Yes', 'Sometime Irregular', 'aaaaaaaaaa', 'Website Development', 'aaaaaaaaa', '', '', '', '', 'Job Holder', 'aaaaaaaaaaaaaaa', 'aaaa', '12', 0.00, 12000.00, 0.00, 0.00, '2025-06-03 20:13:02', 'Bank Amount', '', '', ''),
 ('a79317cf-a918-4a66-bf0e-7be2c19c8c75', 'Govt. Employee', 'Tausif', '01646962631', 8000.00, '1 month', 'goran', 'No', '', '', 'tech', 'jr dev', 'kuril', '7', '', '', NULL, '', '', '', 0.00, 10000.00, 0.00, 0.00, '2025-06-03 09:25:01', 'Bank Amount', 'arbeit', '', ''),
-('cc4e1c24-2c67-4f6b-b3f4-8e3fe5017814', 'Govt. Employee', 'Tausif', '01646962631', 8000.00, '1 month', 'goran', 'Yes', 'Sometime Irregular', 'ok', 'tech', 'jr dev', 'kuril', '7', '', '', NULL, '', '', '', 0.00, 10000.00, 0.00, 0.00, '2025-06-03 09:25:13', 'Bank Amount', 'arbeit', '', '');
+('a7edda74-6982-48f0-baef-18a2234bb168', 'Private Job Holder', 'Tausifur Rahman', '01552577108', 5000.00, '12 month', 'Goran', 'No', '', 'Hello vaiya', 'Website Development', 'Jr. developer', '', '', '', 'Kuril', NULL, '', '', '', 0.00, 0.00, 10000.00, 0.00, '2025-06-04 00:10:20', 'Cash Amount', '', '', 'Arbeit Technology'),
+('cc4e1c24-2c67-4f6b-b3f4-8e3fe5017814', 'Govt. Employee', 'Tausif', '01646962631', 8000.00, '1 month', 'goran', 'Yes', 'Sometime Irregular', 'ok', 'tech', 'jr dev', 'kuril', '7', '', '', NULL, '', '', '', 0.00, 10000.00, 0.00, 0.00, '2025-06-03 09:25:13', 'Bank Amount', 'arbeit', '', ''),
+('e42fcee4-5209-4395-86f8-fa9a70887c15', 'Govt. Employee', 'Tausif Rahman', '01552577108', 12345.00, '1 month', 'aq', 'No', '', 'aq', 'aq', 'aq', 'aq', 'aq', '', '', NULL, '', '', '', 0.00, 123.00, 0.00, 0.00, '2025-06-04 01:24:54', 'Bank Amount', 'aq', '', '');
 
 -- --------------------------------------------------------
 
